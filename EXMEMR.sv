e0f4a4e78fff78f4f75dafee1de9be5157bbf5f6
Parth Sharma <mastery2305@gmail.com>
2026-01-18 17:53:57 +0530

    Removed Read Memory Enable Signal, Used WE3 and WE for Register File and Data Memory respectively

--- EXMEMR.sv
+++ EXMEMR.sv
@@ -5,18 +5,16 @@ module EXMEMRegister import Pkg::* (
     input  logic [31:0] RD2_e,
     input  logic [31:0] PC4_e,
     input  logic [4:0]  rd_e,
-    input  logic        MemR_e,
-    input  logic        MemW_e,
-    input  logic        RegW_e,
+    input  logic        WE_e
+    input  logic        WE3_e
     input  result_mux_t ResultSelect_e,
     input  logic [1:0]  PCNext_select_e,
     output logic [31:0] ALUResult_m,
     output logic [31:0] StoreData_m,
     output logic [31:0] PC4_m,
     output logic [4:0]  rd_m,
-    output logic        MemR_m,
-    output logic        MemW_m,
-    output logic        RegW_m,
+    output logic        WE_m
+    output logic        WE3_m,
     output result_mux_t ResultSelect_m,
     output logic [1:0]  PCNext_select_m
 );
@@ -26,10 +24,9 @@ always_ff @(posedge clk) begin
         ALUResult_m      <= 0;
         StoreData_m      <= 0;
         PC4_m            <= 0;
-        rd_m             <= 0;
-        MemR_m           <= 0;
-        MemW_m           <= 0;
-        RegW_m           <= 0;
+        rd_m             <= 0;;
+        WE_m             <= 0;
+        WE3_m            <= 0;
         ResultSelect_m   <= RESULT_ALU;
         PCNext_select_m  <= STEP_FORWARD;
     end else begin
@@ -37,9 +34,8 @@ always_ff @(posedge clk) begin
         StoreData_m      <= RD2_e;
         PC4_m            <= PC4_e;
         rd_m             <= rd_e;
-        MemR_m           <= MemR_e;
-        MemW_m           <= MemW_e;
-        RegW_m           <= RegW_e;
+        WE_m             <= WE_e;
+        WE3_m            <= WE3_e;
         ResultSelect_m   <= ResultSelect_e;
         PCNext_select_m  <= PCNext_select_e;
     end
