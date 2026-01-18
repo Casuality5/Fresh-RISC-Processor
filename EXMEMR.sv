37feab2b952e4e34a7ade09f2ebd6e06ead2bee7
Parth Sharma <mastery2305@gmail.com>
2026-01-18 18:03:18 +0530

    Changed StoreData keyword to WD

--- EXMEMR.sv
+++ EXMEMR.sv
@@ -10,7 +10,7 @@ module EXMEMRegister import Pkg::* (
     input  result_mux_t ResultSelect_e,
     input  logic [1:0]  PCNext_select_e,
     output logic [31:0] ALUResult_m,
-    output logic [31:0] StoreData_m,
+    output logic [31:0] WD_m,
     output logic [31:0] PC4_m,
     output logic [4:0]  rd_m,
     output logic        WE_m
@@ -22,7 +22,7 @@ module EXMEMRegister import Pkg::* (
 always_ff @(posedge clk) begin
     if (reset) begin
         ALUResult_m      <= 0;
-        StoreData_m      <= 0;
+        WD_m             <= 0;
         PC4_m            <= 0;
         rd_m             <= 0;;
         WE_m             <= 0;
@@ -31,7 +31,7 @@ always_ff @(posedge clk) begin
         PCNext_select_m  <= STEP_FORWARD;
     end else begin
         ALUResult_m      <= ALUResult_e;
-        StoreData_m      <= RD2_e;
+        WD_m             <= RD2_e;
         PC4_m            <= PC4_e;
         rd_m             <= rd_e;
         WE_m             <= WE_e;
