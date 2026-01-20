17ac4c0e5bc5120233b29853a40509d9ea329b50
Parth Sharma <mastery2305@gmail.com>
2026-01-20 18:36:00 +0530

    Minor Adjustments

--- EXMEMR.sv
+++ EXMEMR.sv
@@ -16,19 +16,21 @@ module EXMEMRegister import Pkg::* (
 
 always_ff @(posedge clk) begin
     if (reset) begin
-        ALUResult_m             <= 0;
+        DataMemoryAddress_e     <= 0;
+        Target_Address_m        <= 0;
         WD_m                    <= 0;
         PC4_m                   <= 0;
-        ctrl.MemW_m             <= 0;
-        ctrl.ResultSelect_m     <= RESULT_ALU;
+        ctrl_m.MemW            <= 0;
+        ctrl_m.ResultSelect     <= RESULT_ALU;
         PCNext_select_m         <= STEP_FORWARD;
     end else begin
-        ALUResult_m      <= ALUResult_e;
-        WD_m             <= WD_e;
-        PC4_m            <= PC4_e;
-        ctrl_m.MemW           <= ctrl_e.MemW
-        ctrl_m.ResultSelect   <= ctrl_e.ResultSelect;
-        PCNext_select_m  <= PCNext_select_e;
+        DataMemoryAddress_m      <= DataMemoryAddress_e;
+        Target_Address_m         <= Target_Address_e
+        WD_m                     <= WD_e;
+        PC4_m                    <= PC4_e;
+        ctrl_m.MemW              <= ctrl_e.MemW
+        ctrl_m.ResultSelect      <= ctrl_e.ResultSelect;
+        PCNext_select_m          <= PCNext_select_e;
     end
 end
 endmodule
