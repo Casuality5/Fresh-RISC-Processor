7bb461d46f3ddc20d8f25f06a65b5e4b02c9b963
Parth Sharma <mastery2305@gmail.com>
2026-01-20 19:18:43 +0530

    Did changes as per Vivado's Warning

--- EXMEMR.sv
+++ EXMEMR.sv
@@ -1,10 +1,10 @@
-module EXMEMRegister import Pkg::* (
+module EXMEMRegister import Pkg::*;(
     input  logic clk,
     input  logic reset,
     input logic [31:0] DataMemoryAddress_e,
     input logic [31:0] Target_Address_e,
     input logic [31:0] PC4_e,
-    input logic [31:0] WD_e
+    input logic [31:0] WD_e,
     input bundle_decode_t ctrl_e,
     input logic [31:0] instr_e,
     output logic [31:0] DataMemoryAddress_m,
@@ -16,19 +16,16 @@ module EXMEMRegister import Pkg::* (
 
 always_ff @(posedge clk) begin
     if (reset) begin
-        DataMemoryAddress_e     <= 0;
+        DataMemoryAddress_m     <= 0;
         Target_Address_m        <= 0;
         WD_m                    <= 0;
         PC4_m                   <= 0;
         ctrl_m                  <= 0;
-        PCNext_select_m         <= STEP_FORWARD;
     end else begin
         DataMemoryAddress_m      <= DataMemoryAddress_e;
-        Target_Address_m         <= Target_Address_e
-        WD_m                     <= WD_e;
+        Target_Address_m         <= Target_Address_e;
         PC4_m                    <= PC4_e;
         ctrl_m                   <= ctrl_e;
-        PCNext_select_m          <= PCNext_select_e;
     end
 end
 endmodule
