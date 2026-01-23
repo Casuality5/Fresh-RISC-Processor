052c23e8ad2b310a4e1f4c956d892ed5a7617d02
Parth Sharma <mastery2305@gmail.com>
2026-01-23 15:24:44 +0530

    Vivado Compatible Fetch Stage

--- IF.sv
+++ IF.sv
@@ -1,76 +1,55 @@
 import Pkg::*;
 
-module ProgramCounterMux(
+module Fetch #(
+    parameter Size = 1024
+)(
     input logic            clk,
     input logic            rst,
-    input Decode_Bundle    DB,
     input Execute_Bundle   EB,
-    input Memory_Bundle    MB,
-    input WriteBack_Bundle WB,
     output Fetch_Bundle    FB
 );
 
+logic [31:0] pc_next;
+logic [31:0] address;
+logic [31:0] pc4;
+logic [31:0] instr_out;
+logic [31:0] im[Size-1:0];
+
+assign pc4 = address + 32'h4;
+
 always_comb begin 
-    case (DB.PCNext_Select)
-        STEP_FORWARD:                       FB.PCNext = FB.PC4;
+    case (EB.PCNext_Select)
+        STEP_FORWARD:                       pc_next = pc4;
 
-        JUMP_TO_CALCULATED_REGISTER:        FB.PCNext = EB.ALUResult;
+        JUMP_TO_CALCULATED_REGISTER:        pc_next = EB.ALUResult;
 
-        JUMP_TO_LABEL:                      FB.PCNext = EB.Target_Address;
+        JUMP_TO_LABEL:                      pc_next = EB.Target_Address;
 
-        default:                            FB.PCNext = FB.PC4;
+        default:                            pc_next = pc4;
     endcase
 end
-endmodule
 
-module ProgramCounter (
-    input  logic                 clk,
-    input  logic                 reset,
-    input  Decode_Bundle         DB,
-    input  Execute_Bundle        EB,
-    input  Memory_Bundle         MB,
-    input  WriteBack_Bundle      WB,
-    output Fetch_Bundle          FB
-);
 
-always_ff @ (posedge clk or posedge reset) begin 
-    if (reset) begin 
-        FB.Address <= 32'b0;
+always_ff @ (posedge clk or posedge rst) begin 
+    if (rst) begin 
+        address <= 32'b0;
     end
 
     else begin 
-        FB.Address <= FB.PCNext;
+        address <= pc_next;
     end
 end
-endmodule
-
-module PCPlus4(
-    input  Decode_Bundle    DB,
-    input  Execute_Bundle   EB,
-    input  Memory_Bundle    MB,
-    input  WriteBack_Bundle WB,
-    output Fetch_Bundle     FB
-);
-
-assign FB.PC4 = FB.Address + 32'h4;
-
-endmodule
-
-module InstructionMemory #(
-    parameter Size = 1024
-)(  input Decode_Bundle    DB,
-    input Execute_Bundle   EB,
-    input Memory_Bundle    MB,
-    input WriteBack_Bundle WB,
-    output Fetch_Bundle    FB
-);
-
-logic [31:0] im[Size-1:0];
 
 initial begin
     $readmemh("C:/Users/creat/RV32I/RV32I.srcs/sim_1/new/Program.mem",im);
 end
 
-assign FB.instr = im[FB.Address[31:2] % Size];
+assign instr_out = im[address[31:2] % Size];
 
+
+
+assign FB.PCNext = pc_next;
+assign FB.Address = address;
+assign FB.PC4 = pc4;
+assign FB.instr = instr_out;
 endmodule
