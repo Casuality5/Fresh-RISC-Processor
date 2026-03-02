2d598d3813106ea8a42fba8e400a8a93b162fddb
Parth Sharma <mastery2305@gmail.com>
2026-03-02 20:03:59 +0530

    Updated

--- RV32_LOCKSTEP.py
+++ RV32_LOCKSTEP.py
@@ -29,6 +29,7 @@ with open("SPIKE_TRACE.log",'r') as file:
 
    with open("SPIKE_TRACE.log",'a') as spike_file:
       for line in lines[6:]:
+         machine_code.append(line[26:34])
          line = line.replace(line[23:35],"")
          line = line.replace("  "," ")
          line = line.replace("x","")
@@ -41,21 +42,35 @@ with open("SPIKE_TRACE.log",'r') as file:
 
    with open("SPIKE_TRACE.log",'w') as file:
       file.writelines(lines)
-        
+   
+   with open("SPIKE_TRACE.log",'r') as spike_log:
+      lines = spike_log.readlines()
+      for line in lines:
+         spike_list.append(line)
+
+with open("Program.mem",'a') as file:
+   for instruction in machine_code:
+      file.write(f"{instruction}\n")
+
+VIVADO_BIN = r"C:/AMDDesignTools/2025.2/Vivado/bin"
+SIM_DIR = r"C:/Users/creat/Fresh/Fresh.sim/sim_1/behav/xsim"
+subprocess.run(
+    [VIVADO_BIN + r"/xsim.bat", "tb_behav", "-runall"],
+    cwd=SIM_DIR,
+    check=True
+)
+
 with open("C:/Users/creat/Desktop/dut_trace.log",'r') as vivado_log:
    lines = vivado_log.readlines()
    for line in lines:
       vivado_list.append(line)
-
-with open("SPIKE_TRACE.log",'r') as spike_log:
-   lines = spike_log.readlines()
-   for line in lines:
-      spike_list.append(line)
-      machine_code.append(line[26:34])
 
 for i in range(0,len(machine_code)):
    if vivado_list[i] == spike_list[i]:
       print(f"Instruction:- {machine_code[i]} => Pass")
    else:
       print(f"Instruction:- {machine_code[i]} => Fail")
+
+
+
 
