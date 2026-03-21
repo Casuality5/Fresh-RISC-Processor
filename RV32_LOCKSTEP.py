8fc5619a43b013043c233818274033fa490405bc
Parth Sharma <mastery2305@gmail.com>
2026-03-21 18:09:50 +0530

    Covers R-type

--- RV32_LOCKSTEP.py
+++ RV32_LOCKSTEP.py
@@ -1,5 +1,5 @@
 # step 1. Create Asm file :- nano test.S
-# step 2. Compile to ELF :- riscv64-unknown-elf-gcc -march=rv32i -mabi=ilp32 -nostdlib -Ttext 0x80000000 -o test.elf test.S
+# step 2. Compile to ELF :- riscv64-unknown-elf-gcc -march=rv32i -mabi=ilp32 -nostdlib -Ttext 0x80000000 -o focus.elf focus.S
 # step 3. Generate Hardware Hex File :- riscv64-unknown-elf-objcopy -O verilog --change-addresses -0x80000000 test.elf testvector.mem
 # step 4. Run Spike :- spike --isa=rv32i -m0x7fe00000:0x2000000 --pc=0x80000000 --log-commits --instructions=8 test.elf > spike_final.log 2>&1
 
@@ -8,6 +8,8 @@ import subprocess
 VIV_LIST=[]
 SPK_LIST=[]
 MAC_CODE=[]
+PASS_COUNT = 0
+FAIL_COUNT = 0
 
 INST_COUNT = int(input("Instructions:- "))
 
@@ -34,7 +36,9 @@ with open("spike_final.log",'r') as file:
          line = line.replace("  "," ")
          line = line.replace("0x","")
          line = line.replace("x","")
-         spike_file.write((line[11:])) 
+         if len(line) == 31:
+            line = line[:20] + "0" + line[20:] # Fragile Area
+         spike_file.write((line[11:]))
 
    with open("spike_final.log",'r') as file:
       lines = file.readlines() 
@@ -66,10 +70,15 @@ with open("C:/Users/creat/Desktop/dut_trace.log",'r') as vivado_log:
    lines = vivado_log.readlines()
    for line in lines:
       VIV_LIST.append(line)
+# print(VIV_LIST,len(VIV_LIST))
+# print(SPK_LIST,len(SPK_LIST))
 
 for i in range(0,len(VIV_LIST)):
    if (VIV_LIST)[i] == SPK_LIST[i]:
       print(f"Instruction:- {MAC_CODE[i]} => Pass")
+      PASS_COUNT += 1
    else:
       print(f"Instruction:- {MAC_CODE[i]} => Fail")
+      FAIL_COUNT += 1
 
+print(f"PASS => {PASS_COUNT}, FAIL => {FAIL_COUNT}, MACHINE CODES => {len(MAC_CODE)}")
